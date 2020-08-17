e5657a08f417745cdb85e067880f001bf4861515
WaltherJoander <68170420+Walther17@users.noreply.github.com>
2020-08-17 16:51:49 -0500

    Add files via upload

---
+++ Main7.java
@@ -0,0 +1,21 @@
+package Tv;
+
+public class Main7 {
+	public static void main(String[] args) {
+		Tv tv1;
+		tv1 = new Tv();
+		
+		tv1.encender();
+		System.out.println("Encendido:" + tv1.encendido);
+		
+		tv1.subirvolumen();
+		tv1.subirvolumen();
+		tv1.subirvolumen();
+		tv1.subirvolumen();
+		tv1.subirvolumen();
+		tv1.bajarvolumen();
+		System.out.println("Volumen:" + tv1.encendido);
+	}
+
+
+}
